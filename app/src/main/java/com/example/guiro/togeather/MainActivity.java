bbf49326dec715aa4c6cc13e4c656a756f436607
guirodriguezz <32783994+guirodriguezz@users.noreply.github.com>
2019-04-15 22:28:43 -0300

    Criação das telas de introdução, sliders

--- app/src/main/java/com/example/guiro/togeather/MainActivity.java
+++ app/src/main/java/com/example/guiro/togeather/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
-                .canGoForward(false)
                 .build());
     }
 }
