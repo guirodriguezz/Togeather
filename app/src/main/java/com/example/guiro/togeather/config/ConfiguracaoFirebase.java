4e536029cd45a8cbf3c7a7d09056e3914ad54677
guirodriguezz <32783994+guirodriguezz@users.noreply.github.com>
2019-04-17 03:56:50 -0300

    Finalização das telas de login e cadastro

---
+++ app/src/main/java/com/example/guiro/togeather/config/ConfiguracaoFirebase.java
@@ -0,0 +1,22 @@
+package com.example.guiro.togeather.config;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ConfiguracaoFirebase {
+
+    private static FirebaseAuth autenticacao;
+
+    // Retorna a instancia do FirebaseAuth
+    public static FirebaseAuth getFirebaseAutenticacao() {
+
+        if (autenticacao == null){
+
+            autenticacao = FirebaseAuth.getInstance();
+
+        }
+
+        return autenticacao;
+
+
+    }
+}
