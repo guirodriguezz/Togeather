7ee1c6d1d738183b68969c540e675fc40c37d6fe
guirodriguezz <32783994+guirodriguezz@users.noreply.github.com>
2019-06-01 03:00:39 -0300

    Adição de foto na requisição

--- app/src/main/java/com/example/guiro/togeather/adapter/RequisicoesAdapter.java
+++ app/src/main/java/com/example/guiro/togeather/adapter/RequisicoesAdapter.java
@@ -1,6 +1,7 @@
 package com.example.guiro.togeather.adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.guiro.togeather.R;
 import com.example.guiro.togeather.model.Requisicao;
 import com.example.guiro.togeather.model.Usuario;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RequisicoesAdapter extends RecyclerView.Adapter<RequisicoesAdapter.MyViewHolder> {
 
@@ -43,6 +47,15 @@ public class RequisicoesAdapter extends RecyclerView.Adapter<RequisicoesAdapter.
         holder.nome.setText(usuario.getNome());
         holder.distancia.setText("1 km- aproximadamente");
 
+        if(usuario.getFoto() != null){
+
+            Uri uri = Uri.parse(usuario.getFoto());
+            Glide.with(context).load(uri).into(holder.foto);
+        }else {
+
+            holder.foto.setImageResource(R.drawable.padrao);
+        }
+
     }
 
     @Override
@@ -52,6 +65,7 @@ public class RequisicoesAdapter extends RecyclerView.Adapter<RequisicoesAdapter.
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
+        CircleImageView foto;
         TextView nome, distancia;
 
         public MyViewHolder(View itemView){
@@ -59,6 +73,7 @@ public class RequisicoesAdapter extends RecyclerView.Adapter<RequisicoesAdapter.
 
             nome = itemView.findViewById(R.id.textRequisicaoNome);
             distancia = itemView.findViewById(R.id.textRequisicaoDistancia);
+            foto = itemView.findViewById(R.id.imageViewFotoContato);
         }
     }
 }
